10bb72e6399de5c149c7a991b7acaea11a12b016
Takashi Kato <ktakashi@ymail.com>
2026-04-14 15:53:30 +0200

    Fixing compilation error...

--- berrycrush/junit/src/test/kotlin/org/berrycrush/junit/engine/BerryCrushConfigurationTest.kt
+++ berrycrush/junit/src/test/kotlin/org/berrycrush/junit/engine/BerryCrushConfigurationTest.kt
@@ -1,6 +1,6 @@
 package org.berrycrush.junit.engine
 
-import org.berrycrush.config.BerryCrushConfiguration
+import org.berrycrush.config.BerryCrushConfiguration as Configuration
 import org.berrycrush.junit.BerryCrushBindings
 import org.berrycrush.junit.BerryCrushConfiguration
 import org.junit.jupiter.api.Test
@@ -55,7 +55,7 @@ class TestBindings : BerryCrushBindings {
 
     override fun getOpenApiSpec(): String = "custom-spec.yaml"
 
-    override fun configure(config: BerryCrushConfiguration) {
+    override fun configure(config: Configuration) {
         config.baseUrl = "http://localhost:8080"
     }
 }
